9d63c9b1685af061673dd1baa15bacdd19bed610
Krzysztof Nofz <knofz@virtuslab.com>
2021-08-21 13:29:00 +0200

    add_example_app added styled components

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,3 +1,9 @@
+import styled from 'styled-components';
+
 export const App: React.FC = () => {
-  return <div>App</div>;
+  return <ContainerStyled>App</ContainerStyled>;
 };
+
+const ContainerStyled = styled.div`
+  border: 1px solid red;
+`;
